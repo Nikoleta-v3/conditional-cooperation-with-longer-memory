e432280b52baa4760f14c589b2ac8b00568484c9
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2021-12-14 14:08:44 +0100

    run for different seeds

--- evolRun.m
+++ evolRun.m
@@ -1,15 +1,15 @@
-function evolRun();
+function evolRunReactive();
 
 starting_resident = [0, 0, 0, 0];
 N = 100;
-beta = 0.1;
+beta = 1;
 b=1;
 numberIterations = 10 ^ 7;
 
-cs = linspace(0, 1, 11);
-seed = 1;
+seeds = linspace(0, 10, 11);
+c = 0.3;
 parfor (i = 1:11)
-    c = cs(i);
+    seed = seeds(i)
     [xDat]=evolSimulation(N, c, b, beta, numberIterations, starting_resident, seed);
 end
 end
