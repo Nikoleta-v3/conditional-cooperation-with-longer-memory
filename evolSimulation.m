a7c3a3b9b9fd0fe5a3ae283db97665a16721256f
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-01-05 13:33:20 +0100

    clean matlab code
    Use a single file to run simulations and for stationary
    create different files for the transition matrix M.

--- evolSimulation.m
+++ evolSimulation.m
@@ -1,23 +1,21 @@
-function [xDat]=evolSimulation(N, c, b, beta, numberIterations, starting_resident, seed);
+function [xDat]=evolSimulation(N, c, b, beta, numberIterations, starting_resident, seed, sdim);
 rng(seed)
 %% Preparations for the output
 Data=['c=',num2str(c),'; b=',num2str(b),'; N=',num2str(N), '; beta=',num2str(beta), '; nIt=',num2str(numberIterations)];
 AvCoop=0; AvPay=0; Res=starting_resident;
-filename = "data/matlab_two_bits_beta_" + beta + "_seed_" + seed + "_c_" + c;
+filename = "data/matlab_bits_" + sdim + "_beta_" + beta + "_seed_" + seed + "_c_" + c;
 
 %% Initialization
-sdim=4;
-xDat=zeros(numberIterations/100, 6);
+xDat=zeros(numberIterations/100, sdim + 2);
 xDat(1,:)=[Res, 0, 0];
 
-
-u = [b - c, -c, b, 0, b - c, -c, b, 0, b - c, -c, b, 0, b - c, -c, b, 0];
+u = repmat([b - c, -c, b, 0], 1, (sdim ^ 2) / 4);
 
 %% Running the evolutionary process
 j = 2;
 for t = progress(1:numberIterations)
     Mut=rand(1, sdim);
-    [phi, coopM, piM]=calcPhi(Mut, Res, N, u, beta);
+    [phi, coopM, piM]=calcPhi(Mut, Res, N, u, beta, sdim);
     if rand(1) < phi
         Res=Mut; xDat(j,:)=[Res, t, coopM]; j=j+1;
     end
@@ -25,22 +23,18 @@ end
 
 dlmwrite(filename + ".csv", xDat, 'precision', 9);
 writematrix(Data, filename + ".txt");
-
-AvCoop = mean(xDat(:,end-2));
-AvPay = mean(xDat(:,end-1));
 end
 
-function [phi, coopMM, piMM]=calcPhi(Mut, Res, N, u, beta);
+function [phi, coopMM, piMM]=calcPhi(Mut, Res, N, u, beta, sdim);
 %% Calculating the fixation probability
 
-vMM=stationary(Mut, Mut);
-vMR=stationary(Mut, Res);
-vRM=stationary(Res, Mut);
-vRR=stationary(Res, Res);
+vMM=stationary(Mut, Mut, sdim);
+vMR=stationary(Mut, Res, sdim);
+vRM=stationary(Res, Mut, sdim);
+vRR=stationary(Res, Res, sdim);
 
 piMM=vMM*u';
-coopMM=vMM(1) + vMM(5) + vMM(9) + vMM(13) + vMM(2) + vMM(6) + vMM(10) + vMM(14);
-
+coopMM= sum(vMM(2:4:end)) + sum(vMM(1:4:end));
 
 piMR=vMR*u';
 piRM=vRM*u';
