6c35868027b8f0c723b7167e69a9b01263c780d3
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2022-07-04 10:56:22 +0200

    matlab file for Vaquero

--- src/evolutionary_simulations/cooperationRates.m
+++ src/evolutionary_simulations/cooperationRates.m
@@ -2,9 +2,9 @@ function data=cooperationRates(sdim, epsilon);
 
 strategies = dec2bin(0:2 ^ sdim - 1, sdim) - '0'; 
 
-for i=progress(1:sdim ^ 2)
-    data = zeros(sdim ^ 2, 7 + (sdim) * 2);
-    for j=(1:sdim^2)
+for i=progress(1:2 ^ sdim)
+    data = zeros(2 ^ sdim, 7 + (sdim) * 2);
+    for j=(1:2^sdim)
         p = (1 - epsilon) * strategies(i, :) + epsilon * (1 - strategies(i, :));
         q = (1 - epsilon) * strategies(j, :) + epsilon * (1 - strategies(j, :));
         v = stationary(p, q, sdim);
@@ -12,6 +12,6 @@ for i=progress(1:sdim ^ 2)
         xDC=sum(v(3:4:end)); xDD=sum(v(4:4:end));
         data(j,:) = [i, j, strategies(i, :), strategies(j, :), epsilon, xCC, xCD, xDC, xDD];
     end
-    filename = "cooperation_data/error_" + epsilon + "sdim_" + sdim + "_s" + i + ".csv";
+    filename = "cooperation_data/error_" + epsilon + "_sdim_" + sdim + "_s" + i + ".csv";
     dlmwrite(filename, data, 'precision', 9);
 end
