dab1cb270ea26bfb029e02b7ba41d23a8a1ca9d4
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2024-04-01 23:48:12 +0200

    prep to run in parallel

--- src/evolution_simulations/evolRun.m
+++ src/evolution_simulations/evolRun.m
@@ -5,7 +5,7 @@ sdims = [2; 4];
 
 N = 100;
 b = 1;
-numberIterations = 10 ^ 3;
+numberIterations = 10 ^ 7;
 seed = 1;
 errorprobabilities = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4];
 
@@ -14,7 +14,7 @@ c = 0.3;
 
 filenames = ["ErrorSim/reactive_one_error_"; "ErrorSim/reactive_two_error_"];
 
-for i=1:2
+parfor i=1:2
     for j=1:7 
     sdim = sdims(i);
     errorprobability = errorprobabilities(j);
