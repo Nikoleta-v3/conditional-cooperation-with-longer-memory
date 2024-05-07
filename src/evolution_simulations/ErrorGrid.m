6b5d08430e76551098a97b40ba186604de68deb3
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2024-05-07 12:52:38 +0200

    prep to run more seeded simulations

--- src/evolution_simulations/ErrorGrid.m
+++ src/evolution_simulations/ErrorGrid.m
@@ -1,4 +1,4 @@
-function ErrorGrid(sdim);
+function ErrorGrid(index);
 
 error_probabilities = -5:0.5:-1; cost_values = 0.1:0.1:1;
 num_error_values = 9; num_cost_values = 10;
@@ -12,16 +12,17 @@ num_error_values = 9; num_cost_values = 10;
 %         parameters(index, 2) = cost_values(j);
 %     end
 % end
-errorprobability = 10 ^ error_probabilities(1);
+errorprobability = 10 ^ error_probabilities(index);
 N = 100;
 b = 1;
-numberIterations = 10 ^ 3;
+numberIterations = 10 ^ 7;
 beta = 1;
+sdim = 2;
 starting_resident = zeros(1, sdim);
 
 folder = "ErrorGrid/dimension_";
 
-parfor i=1:num_cost_values
+for i=8:num_cost_values
     seed = i;
     c = cost_values(i);
     filename = append(folder, num2str(sdim), "_error_", num2str(errorprobability), "_cost_", num2str(c));
--- src/evolution_simulations/evolRun.m
+++ src/evolution_simulations/evolRun.m
@@ -7,20 +7,20 @@ N = 100;
 b = 1;
 numberIterations = 10 ^ 7;
 seed = 1;
-errorprobabilities = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4];
+% errorprobabilities = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4];
 
 beta = 1;
 c = 0.3;
 
-filenames = ["ErrorSim/reactive_one_error_"; "ErrorSim/reactive_two_error_"];
+filename = "reactive_three_no_error";
 
-parfor i=1:2
-    for j=1:7 
-    sdim = sdims(i);
-    errorprobability = errorprobabilities(j);
-    starting_resident = zeros(1, sdim);
-    filename = append(filenames(i), num2str(errorprobability));
-    [xDat]=evolSimulation(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
-    end
-end
+% parfor i=1:2
+%     for j=1:7 
+sdim = 8;
+errorprobability = 0;
+starting_resident = zeros(1, sdim);
+% filename = append(filenames(i), num2str(errorprobability));
+[xDat]=evolSimulation(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
+%     end
+% end
 end
