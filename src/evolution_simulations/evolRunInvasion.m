b9e623162b638f52928123e799d1e5a0ddb60052
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2024-07-21 16:29:22 +0200

    working on revisions

--- src/evolution_simulations/evolRunInvasion.m
+++ src/evolution_simulations/evolRunInvasion.m
@@ -4,15 +4,15 @@ sdim = 4;
 
 N = 100;
 b = 1;
-numberIterations = 10 ^ 3;
+numberIterations = 10 ^ 4;
 seed = 1;
 beta = 1;
 c = 0.5;
 errorprobability = 0;
 
-residents = [[1, .05, .6, .3]; [1, .6, .05, .3]; [1, .05, .6, .1]; [1, .6, .05, .1]];
+residents = [[0.99, .1, .6, .3]; [0.99, .6, .1, .3]];
 
-for i=1:4
+for i=1:2
     starting_resident = residents(i, :);
     p1 = starting_resident(1);
     p2 = starting_resident(2);
@@ -21,10 +21,10 @@ for i=1:4
 
 %     % filename = append("Invasion/sdim_2_invasions_resident_p_", num2str(p), '_q_', num2str(q));
     filename = append("Invasion/sdim_4_invasions_resident_p1_", num2str(p1), '_p2_', num2str(p2), '_p3_', num2str(p3), '_p4_', num2str(p4));
-    disp(filename)
-    disp(starting_resident)
+%     disp(filename)
+%     disp(starting_resident)
 % 
-%     [xDat]=Invasion(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
+    [xDat]=Invasion(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
 
 end
 end
