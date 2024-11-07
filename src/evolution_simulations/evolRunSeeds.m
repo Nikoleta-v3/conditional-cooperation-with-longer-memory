a31c8b8325bdab6da456d352e7c0292927a1fce5
Nikoleta-v3 <glynatsi@evolbio.mpg.de>
2024-11-07 15:21:30 +0100

    use parfor

--- src/evolution_simulations/evolRunSeeds.m
+++ src/evolution_simulations/evolRunSeeds.m
@@ -1,5 +1,5 @@
 % Script to run the evolutionary process for a range of seeds
-function evolRunSeeds(seed);
+function evolRunSeeds();
 
 sdim = 4;
 starting_resident = zeros(1, sdim);
@@ -11,13 +11,13 @@ beta = 1;
 c = .5;
 % seeds = linspace(1, 10, 10);
 errorprobability = 0;
+% 
+% filename = append("test_with_mutants_take_one_", num2str(seed), ".csv");
+% [xDat]=evolSimulationGrid(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
 
-filename = append("test_with_mutants_take_one_", num2str(seed), ".csv");
-[xDat]=evolSimulationGrid(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
-
-% for (i = 1:2)
-%     seed = seeds(i);
-%     filename = append("test_with_mutants_take_one", num2str(seed), ".csv");
-%     [xDat]=evolSimulationGrid(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
-% %        [xDat]=evolSimulationCounting(N, c, b, beta, numberIterations, starting_resident, seed, sdim,  errorprobability, filename);
-% end
+parfor (i = 1:10)
+    seed = seeds(i);
+    filename = append("test_with_mutants_take_one_", num2str(seed), ".csv");
+    [xDat]=evolSimulationGrid(N, c, b, beta, numberIterations, starting_resident, seed, sdim, errorprobability, filename);
+%        [xDat]=evolSimulationCounting(N, c, b, beta, numberIterations, starting_resident, seed, sdim,  errorprobability, filename);
+end
